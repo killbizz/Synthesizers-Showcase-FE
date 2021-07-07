0424527a239c4ebc9c2f5cc8f87c1be8a5077641
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-07 17:15:03 +0200

    aggiunti valori di default ai parametri del costruttore

--- src/app/classes/Synthesizer.ts
+++ src/app/classes/Synthesizer.ts
@@ -1,13 +1,13 @@
 import { SynthesizerInterface } from './../interfaces/SynthesizerInterface';
 
 export class Synthesizer implements SynthesizerInterface{
-    name!: string;
-    description!: string;
-    category!: string;
-    price!: number;
+    name: string;
+    description: string;
+    category: string;
+    price: number;
     base64image: string | undefined;
 
-    constructor(name: string, description: string, category: string, price: number, base64image: string | undefined){
+    constructor(name: string = "", description: string = "", category: string = "", price: number = 0, base64image: string | undefined){
         this.name = name;
         this.description = description;
         this.category = category;
