bc5d99b57839e5e8a914f47b10955a7f4f493214
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-08 18:30:40 +0200

    aggiunta gestione autenticazione su navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  userLogged: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.authService.userSignedIn.subscribe(() => {
+      this.userLogged = true;
+    });
+    this.authService.userLoggedOut.subscribe(() => {
+      this.userLogged = false;
+    });
+  }
 
   ngOnInit(): void {
+    this.userLogged = this.authService.isUserLoggedIn();
+  }
+
+  login(event: any){
+    event.preventDefault();
+    this.router.navigateByUrl('login');
+  }
+
+  logout(event: any){
+    event.preventDefault();
+    this.authService.logout();
+    this.router.navigateByUrl('');
   }
 
 }
